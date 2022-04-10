b42f35fa85cf017b162da8edbdaa141251480160
bxneter <bxneter@gmail.com>
2022-04-11 02:52:27 +0700

    Add threshold and symbols configuration params

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
-pub const SIGNAL_THRESHOLD_ASK: f32 = 0.002;
-pub const SIGNAL_THRESHOLD_BID: f32 = 0.002;
-pub const SYMBOLS: &'static str = "btc,eth,dot,powr,evx,mana,near";
+pub const SIGNAL_THRESHOLD_ASK: f32 = 0.005;
+pub const SIGNAL_THRESHOLD_BID: f32 = 0.005;
+pub const SYMBOLS: &'static str =
+  "btc,eth,dot,powr,evx,mana,near,zil,doge,bnb,iost,sand,gala,sol,avax";
