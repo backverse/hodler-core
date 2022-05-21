6188784d10f98c06c57ffba3654d091b8051a313
bxneter <bxneter@gmail.com>
2022-05-21 13:25:48 +0700

    Increase arbitrage threshold to 0.025

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,4 +1,4 @@
 pub const PORT: u16 = 3000;
 pub const DEFAULT_LOGGING_LEVEL: &str = "info";
-pub const SIGNAL_THRESHOLD: f32 = 0.01;
+pub const SIGNAL_THRESHOLD: f32 = 0.025;
 pub const SYMBOLS: &str = "btc,eth,dot,powr,evx,mana,near,zil,doge,bnb,iost,sand,gala,sol,avax";
