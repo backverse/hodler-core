08cfffd38e2c9206dc69868f7e8318fa843e6535
bxneter <bxneter@gmail.com>
2022-06-26 06:05:51 +0700

    Add `target` cleaning before building new app

--- .scripts/start.sh
+++ .scripts/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/hodler-signal/
+~/.cargo/bin/cargo clean
 ~/.cargo/bin/cargo b -r
 sudo pkill signal
 RUST_LOG=debug ~/.cargo/bin/cargo r -r </dev/null &>/var/log/hodler-signal &
