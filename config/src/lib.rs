4de66411e0f4a236954d19e403f4d9fda365c6c9
bxneter <bxneter@gmail.com>
2022-07-03 03:50:40 +0700

    Add more cryptocurrencies

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -2,4 +2,4 @@ pub const PORT: u16 = 3000;
 pub const DEFAULT_LOGGING_LEVEL: &str = "info";
 pub const SIGNAL_THRESHOLD: f32 = 0.025;
 pub const CURRENCY_SYMBOL: &str = "btc";
-pub const SYMBOLS: &str = "btc,eth,dot,powr,ltc,mana,near,zil,doge,bnb,iost,sand,gala,sol,avax";
+pub const SYMBOLS: &str = "btc,eth,wan,ada,omg,bch,usdt,ltc,xrp,zil,snt,cvc,link,iost,zrx,knc,abt,mana,ctxc,xlm,six,jfin,bnb,pow,doge,trx,dai,xtz,usdc,bat,mkr,enj,band,comp,ksm,dot,near,scrt,glm,don,yfi,uni,aave,alpha,ocean,snx,sand,bal,crv,grt,matic,kub,axs,sushi,ftt,imx,dydx,ens,boba,ilv,gala,gt,sgb,lyxe,chz,gf,exfi,sol,avax,ftm,luna,ape,hbar,lrc,celo,gal,solo,luna2,op";
